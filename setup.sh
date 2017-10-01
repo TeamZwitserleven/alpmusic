448db0423ce6821adf5fc552a7bc6b681c5a4fcb
Ewout Prangsma <ewout@prangsma.net>
2017-10-01 14:21:05 +0200

    Script fixes & docs

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ if [ -z "${PIPASSWORD}" ]; then
 fi
 
 MPG123=$(which mpg123)
-if [ -z "${MSG123}" ]; then
+if [ -z "${MPG123}" ]; then
     apt-get update
     apt-get -y install mpg123
     MPG123=$(which mpg123)
@@ -21,7 +21,7 @@ fi
 adduser --gecos $PIUSER --disabled-login $PIUSER --uid 1000
 chown -R 1000:1000 /home/$PIUSER 
 echo "$PIUSER:$PIPASSWORD" | chpasswd 
-usermod -a -G sudo,adm,input,video,plugdev $PIUSER
+usermod -a -G sudo,adm,audio,input,video,plugdev $PIUSER
 
 mkdir -p /music 
 cp -Rf music/*.mp3 /music/
@@ -32,11 +32,35 @@ cat > "/etc/systemd/system/music-player.service" <<EOF
 Description=Play music 
 
 [Service]
+User=${PIUSER}
 ExecStart=/bin/sh -c "mpg123 -Z /music/*.mp3"
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+cat > "/home/$PIUSER/.asoundrc" <<EOF
+pcm.!default {
+ type asym
+ capture.pcm "mic"
+ playback.pcm "speaker"
+}
+pcm.mic {
+ type plug
+ slave {
+ pcm "hw:0,0"
+ }
+}
+pcm.speaker {
+ type plug
+ slave {
+ pcm "hw:0,0"
+ }
+}
+EOF
+
+chown -R 1000:1000 /home/$PIUSER 
+
+systemctl daemon-reload
 systemctl enable music-player.service
 systemctl restart music-player.service
